9eda2687a5edc0fc53cce78dd4aaf4e9cfb0ba19
Ben Chidgey <ben@feedmypixel.com>
2018-02-24 16:48:41 +0000

    Build helper scripts
    Hash assets work
    Nunjucks build from njk to html scripts
    General build script to convert assets and nunjucks into a html
    page with hashed assets

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PATH="/usr/local/bin:$PATH"
+
+set -o errexit
+echo "---- Running Production build"
+
+npm run clean:dist
+npm run build:css && npm run build:html && \
+npm run copy:images && npm run copy:favicon && \
+npm run hash:images && npm run hash:assets
