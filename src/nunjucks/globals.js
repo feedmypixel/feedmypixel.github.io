c8735792e6f76b517f745a5087d9eccd3e64ce9e
Ben Chidgey <ben@feedmypixel.com>
2019-11-03 20:50:31 +0000

    update global property to correct name

--- src/nunjucks/globals.js
+++ src/nunjucks/globals.js
@@ -1,6 +1,6 @@
 let globals = new Map()
 
-globals.set('siteName', 'feedMyPixel - Software solutions - Ben Chidgey')
+globals.set('pageTitle', 'feedMyPixel - Software solutions - Ben Chidgey')
 globals.set('currentYear', (new Date()).getFullYear())
 
 const init = (env) => globals.forEach((value, key) => env.addGlobal(key, value))
