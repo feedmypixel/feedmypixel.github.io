3c4635fd4d2f92a5756b4aad51ebf54a51980c94
Ben Chidgey <ben@feedmypixel.com>
2022-11-12 12:01:32 +0000

    Update document title

--- src/nunjucks/globals.js
+++ src/nunjucks/globals.js
@@ -1,6 +1,9 @@
 let globals = new Map()
 
-globals.set('pageTitle', 'feedMyPixel - Web based things - Ben Chidgey')
+globals.set(
+  'pageTitle',
+  'feedMyPixel - Consultant Software engineer - Ben Chidgey'
+)
 
 const init = (env) => globals.forEach((value, key) => env.addGlobal(key, value))
 
